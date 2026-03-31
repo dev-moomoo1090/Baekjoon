7289a72d9063b4724bb544dabfb22f505efce322
박현수 <dev_hyuns1090@naver.com>
2026-03-31 15:20:15 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <set>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int y = 2024;
+    int m = 8;
+
+    int N; cin >> N;
+    N--;
+    m += 7*N;
+    y += m / 12;
+    m = m % 12;
+
+    cout << (m== 0 ? y - 1 : y) << " "<<  (m == 0 ? 12 : m);
+}
